08087c42918f138def89e2390d24e53157b57694
Franco Valansi <francovalansi@gmail.com>
2026-06-30 09:40:17 -0300

    Agregar funcionalidad de alta de productos
    -Incorporación de formulario para agregar productos
    -Validaciones del formulario
    -Código más ordenado

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,46 @@ import { useEffect, useState } from "react";
 import { supabase } from "./supabase";
 import InventoryTable from "./components/InventoryTable";
 import type { Producto } from "./components/InventoryTable";
+import FormularioProducto from "./components/FormularioProducto";
 
 function App() {
 
   const [productos, setProductos] = useState<Producto[]>([]);
   const [cargando, setCargando] = useState(true);
   const [errorCarga, setErrorCarga] = useState("");
+  const [dialogoAbierto, setDialogoAbierto] = useState(false);
+  const [nombre, setNombre] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [precio, setPrecio] = useState("");
+  const [stock, setStock] = useState("");
+
+  const formularioValido =
+  nombre.trim() !== "" &&
+  precio !== "" &&
+  stock !== "" &&
+  Number(precio) >= 0 &&
+  Number(stock) >= 0;
 
   useEffect(() => {
     obtenerProductos();
   }, []);
+
+  function limpiarFormulario() {
+    setNombre("");
+    setDescripcion("");
+    setPrecio("");
+    setStock("");
+  }
+
+  function abrirFormulario() {
+    limpiarFormulario();
+    setDialogoAbierto(true);
+  }
+
+  function cerrarFormulario() {
+    limpiarFormulario();
+    setDialogoAbierto(false);
+  }
 
   async function obtenerProductos() {
     const { data, error } = await supabase
@@ -27,6 +57,40 @@ function App() {
 
     setCargando(false);
 
+  }
+
+  async function agregarProducto() {
+    if (!formularioValido) {
+    alert("Complete correctamente los datos del producto.");
+    return;
+  }
+    console.log({
+      nombre,
+      descripcion,
+      precio,
+      stock
+    });
+
+
+    const { error } = await supabase
+      .from("productos")
+      .insert([
+        {
+          nombre,
+          descripcion,
+          precio: Number(precio),
+          stock: Number(stock),
+        },
+      ]);
+
+    if (error) {
+      console.error(error);
+      alert("No se pudo agregar el producto.");
+      return;
+    }
+
+    obtenerProductos();
+    cerrarFormulario();
   }
   
   if (cargando) {
@@ -47,7 +111,27 @@ function App() {
 
   return (
     <div className="app">
-      <InventoryTable productos={productos} />
+      <InventoryTable
+        productos={productos}
+        onAgregar={abrirFormulario} />
+
+      <FormularioProducto
+        abierto={dialogoAbierto}
+
+        nombre={nombre}
+        descripcion={descripcion}
+        precio={precio}
+        stock={stock}
+
+        setNombre={setNombre}
+        setDescripcion={setDescripcion}
+        setPrecio={setPrecio}
+        setStock={setStock}
+
+        formularioValido={formularioValido}
+        alCerrar={cerrarFormulario}
+        alGuardar={agregarProducto}
+      />
     </div>
   );
 
---
+++ src/components/FormularioProducto.tsx
@@ -0,0 +1,168 @@
+import "./FormularioProducto.css";
+import { useEffect, useState } from "react";
+import type { FormEvent } from "react";
+
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+} from "@mui/material";
+
+interface FormularioProductoProps {
+  abierto: boolean;
+  nombre: string;
+  descripcion: string;
+  precio: string;
+  stock: string;
+
+  setNombre: React.Dispatch<React.SetStateAction<string>>;
+  setDescripcion: React.Dispatch<React.SetStateAction<string>>;
+  setPrecio: React.Dispatch<React.SetStateAction<string>>;
+  setStock: React.Dispatch<React.SetStateAction<string>>;
+
+  formularioValido: boolean;
+  alCerrar: () => void;
+  alGuardar: () => void;
+}
+
+export default function FormularioProducto({
+  abierto,
+  nombre,
+  descripcion,
+  precio,
+  stock,
+  setNombre,
+  setDescripcion,
+  setPrecio,
+  setStock,
+  formularioValido,
+  alCerrar,
+  alGuardar,
+}: FormularioProductoProps) {
+  const [nombreTocado, setNombreTocado] = useState(false);
+  const [precioTocado, setPrecioTocado] = useState(false);
+  const [stockTocado, setStockTocado] = useState(false);
+
+  useEffect(() => {
+    if (!abierto) {
+      setNombreTocado(false);
+      setPrecioTocado(false);
+      setStockTocado(false);
+    }
+  }, [abierto]);
+
+  function manejarSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    setNombreTocado(true);
+    setPrecioTocado(true);
+    setStockTocado(true);
+
+    if (formularioValido) {
+      alGuardar();
+    }
+  }
+
+  return (
+    <Dialog
+      open={abierto}
+      onClose={alCerrar}
+      fullWidth
+      maxWidth="sm"
+    >
+      <form onSubmit={manejarSubmit}>
+
+        <DialogTitle>
+          Agregar producto
+        </DialogTitle>
+
+        <DialogContent className="formulario-contenido">
+
+          <TextField
+            label="Nombre"
+            value={nombre}
+            onChange={(e) => {
+              setNombre(e.target.value);
+              setNombreTocado(true);
+            }}
+
+            error={nombreTocado && nombre.trim() === ""}
+
+            helperText={
+              nombreTocado && nombre.trim() === ""
+                ? "Ingrese un nombre."
+                : ""
+            }
+            fullWidth
+          />
+
+          <TextField
+            label="Descripción"
+            value={descripcion}
+            onChange={(e) => setDescripcion(e.target.value)}
+            fullWidth
+          />
+
+          <TextField
+            label="Precio"
+            type="number"
+            value={precio}
+            onChange={(e) => {
+              setPrecio(e.target.value);
+              setPrecioTocado(true);
+            }}
+
+            error={precioTocado && (precio === "" || Number(precio) < 0)}
+
+            helperText={
+              precioTocado && (precio === "" || Number(precio) < 0)
+                ? "Ingrese un precio válido."
+                : ""
+            }
+            fullWidth
+          />
+
+          <TextField
+            label="Stock"
+            type="number"
+            value={stock}
+            onChange={(e) => {
+              setStock(e.target.value);
+              setStockTocado(true);
+            }}
+
+            error={stockTocado && (stock === "" || Number(stock) < 0)}
+
+            helperText={
+              stockTocado && (stock === "" || Number(stock) < 0)
+                ? "Ingrese un stock válido."
+                : ""
+            }
+            fullWidth
+          />
+
+        </DialogContent>
+
+        <DialogActions>
+
+          <Button onClick={alCerrar}>
+            Cancelar
+          </Button>
+
+          <Button
+            type="submit"
+            variant="contained"
+            disabled={!formularioValido}
+          >
+            Guardar
+          </Button>
+
+        </DialogActions>
+
+      </form>
+    </Dialog>
+  );
+}
